821dc6fafaa29abedc0192ac3e0b472dd593aa8f
Greg Methvin <greg@methvin.net>
2017-12-10 18:48:25 -0800

    Add some more complex stuff

--- src/main/java/com/example/helloservice/HelloService.java
+++ src/main/java/com/example/helloservice/HelloService.java
@@ -12,13 +12,31 @@ import static com.lightbend.lagom.javadsl.api.Service.pathCall;
  * API for the HelloService. This could be separated into another subproject.
  */
 public interface HelloService extends Service {
-  ServiceCall<NotUsed, String> hello(String id);
+  ServiceCall<NotUsed, WelcomeMessage> hello(String id);
+  ServiceCall<NotUsed, WelcomeMessage> welcome();
+  ServiceCall<NotUsed, CountMessage> count();
+
+  class WelcomeMessage {
+    final String message;
+    WelcomeMessage(String message) {
+      this.message = message;
+    }
+  }
+
+  class CountMessage {
+    final int count;
+    CountMessage(int count) {
+      this.count = count;
+    }
+  }
 
   @Override
   default Descriptor descriptor() {
     return named("hello")
         .withCalls(
-            pathCall("/hello/:id", this::hello)
+            pathCall("/hello/:id", this::hello),
+            pathCall("/", this::welcome),
+            pathCall("/count", this::count)
         )
         .withAutoAcl(true);
   }
--- src/main/java/com/example/helloservice/HelloServiceImpl.java
+++ src/main/java/com/example/helloservice/HelloServiceImpl.java
@@ -1,17 +1,33 @@
 package com.example.helloservice;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.inject.Singleton;
+
 import akka.NotUsed;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
-import java.util.concurrent.CompletableFuture;
 
 /**
  * Implementation of the HelloService.
  */
+@Singleton
 public class HelloServiceImpl implements HelloService {
 
+  private final AtomicInteger counter = new AtomicInteger();
+
   @Override
-  public ServiceCall<NotUsed, String> hello(String id) {
-    return request -> CompletableFuture.completedFuture("Hello, " + id + "!\n");
+  public ServiceCall<NotUsed, WelcomeMessage> hello(String id) {
+    return request -> CompletableFuture.completedFuture(new WelcomeMessage("Hello " + id));
   }
 
+  @Override
+  public ServiceCall<NotUsed, WelcomeMessage> welcome() {
+    return request -> CompletableFuture.completedFuture(new WelcomeMessage("Hello!"));
+  }
+
+  @Override
+  public ServiceCall<NotUsed, CountMessage> count() {
+    return request -> CompletableFuture.completedFuture(new CountMessage(counter.incrementAndGet()));
+  }
 }
